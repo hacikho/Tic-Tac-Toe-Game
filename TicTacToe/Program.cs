e3d4b970f453bab5afbc5d8c73bf883beabec54d
Haci Karahasanoglu <haci@hacikho.com>
2017-09-15 18:27:35 -0400

    this code contains some commented codes.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -14,15 +14,14 @@ namespace TicTacToe
         static bool isWin = false;
         //Display Board numbers
         static string[] tempValueArray = new string[9] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
-
+        static int milliseconds = 500;
         static void Main(string[] args)
         {
             //Main action to get input from player and update borad, and check who win the game
             do
             {
-                int milliseconds = 500;
                 Board();
-                AskPlayer1();
+                AskPlayer(1, "X");
                 Thread.Sleep(milliseconds);
                 Console.Clear();
                 Board();
@@ -30,7 +29,7 @@ namespace TicTacToe
                 {
                     break;
                 }
-                AskPlayer2();
+                AskPlayer(2, "O");
                 Thread.Sleep(milliseconds);
                 Console.Clear();
                 if (CheckForWin())
@@ -40,21 +39,50 @@ namespace TicTacToe
             } while (isWin == false);
         }
 
-        //Ask player 1 to place an X in the board
-        public static void AskPlayer1()
+        //Ask player generic
+        public static void AskPlayer(int playerNumber, string XorO)
         {
-            Console.Write("Player 1 chose a location to place X: ");
-            string player1Location = Console.ReadLine();
-            tempValueArray[int.Parse(player1Location)] = "X";
+            string playerLocation;
+            do
+            {
+                Console.Write($"Player {playerNumber} choose a location to place {XorO} : ");
+                playerLocation = Console.ReadLine();
+            } while (tempValueArray[int.Parse(playerLocation)] == "X" || tempValueArray[int.Parse(playerLocation)] == "O");
+
+            //Console.Write($"Player {playerNumber} choose a location to place {XorY} : ");
+            //string playerLocation = Console.ReadLine();
+            //if(tempValueArray[int.Parse(playerLocation)] == "X" || tempValueArray[int.Parse(playerLocation)] == "Y")
+            //{
+            //    Console.WriteLine("You can not make entry to this location!!!!!");
+            //    Thread.Sleep(milliseconds);
+            //    Thread.Sleep(milliseconds);
+            //    Console.Clear();
+            //    Board();
+            //    AskPlayer(playerNumber, XorY);
+            //}
+
+            tempValueArray[int.Parse(playerLocation)] = XorO;
         }
 
-        //Ask player 2 to place an Y in the board
-        public static void AskPlayer2()
+        public static void callAgain()
         {
-            Console.Write("Player 2 chose a location to place O: ");
-            string player2Location = Console.ReadLine();
-            tempValueArray[int.Parse(player2Location)] = "O";
+
         }
+        ////Ask player 1 to place an X in the board
+        //public static void AskPlayer1()
+        //{
+        //    Console.Write("Player 1 chose a location to place X: ");
+        //    string player1Location = Console.ReadLine();
+        //    tempValueArray[int.Parse(player1Location)] = "X";
+        //}
+
+        ////Ask player 2 to place an Y in the board
+        //public static void AskPlayer2()
+        //{
+        //    Console.Write("Player 2 chose a location to place O: ");
+        //    string player2Location = Console.ReadLine();
+        //    tempValueArray[int.Parse(player2Location)] = "O";
+        //}
 
         //Display Board
         public static void Board()
